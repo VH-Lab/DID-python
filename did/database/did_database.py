2a2d3f58e6d6aff6a43d0123e0a2917ae9e9fefe
David Nishikawa <david@squishymedia.com>
2020-10-14 12:13:28 -0700

    rename ndi_database => did_database

--- did/database/did_database.py
+++ did/database/did_database.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 
 from ..document import Document
 
-class NDI_Database(ABC):
+class DID_Database(ABC):
     """
     Abstract class for NDI database interfaces.
     Child classes of :class:`NDI_Database` are standardized, and share the same base methods and data signatures.
@@ -18,38 +18,38 @@ class NDI_Database(ABC):
         pass
 
     @abstractmethod
-    def add(self, ndi_document):
-        """It should be able to add one or many instances of an :term:`NDI object` to the appropriate :term:`collection`.
+    def add(self, did_document):
+        """It should be able to add one or many instances of an :term:`DIDDocument object` to the appropriate :term:`collection`.
         
-        :param ndi_document:
-        :type ndi_document: :term:`NDI object`
+        :param did_document:
+        :type did_document: 
         """
         pass
 
     @abstractmethod
-    def update(self, ndi_document, force=False):
-        """It should be able to update one or many instances of an :term:`NDI object`. Updated entries are found by their :term:`NDI class` and id.
+    def update(self, did_document, force=False):
+        """It should be able to update one or many instances of an :term:`DIDDocument object`. Updated entries are found by their :term:`NDI class` and id.
         
-        :param ndi_document:
-        :type ndi_document: :term:`NDI object`
+        :param did_document:
+        :type did_document: 
         """
         pass
 
     @abstractmethod
-    def upsert(self, ndi_document, force=False):
-        """It should be able to update one or many instances of an :term:`NDI object`. Updated entries are found by their :term:`NDI class` and id. In the case that an instance does not already exist, it should be added to its :term:`collection`.
+    def upsert(self, did_document, force=False):
+        """It should be able to update one or many instances of an :term:`DIDDocument object`. Updated entries are found by their :term:`NDI class` and id. In the case that an instance does not already exist, it should be added to its :term:`collection`.
         
-        :param ndi_document:
-        :type ndi_document: :term:`NDI object`
+        :param did_document:
+        :type did_document: 
         """
         pass
 
     @abstractmethod
-    def delete(self, ndi_document, force=False):
-        """It should be able to delete one or many instances of an :term:`NDI object`.
+    def delete(self, did_document, force=False):
+        """It should be able to delete one or many instances of an :term:`DIDDocument object`.
         
-        :param ndi_document:
-        :type ndi_document: :term:`NDI object`
+        :param did_document:
+        :type did_document: 
         """
         pass
 
@@ -89,22 +89,22 @@ class NDI_Database(ABC):
         pass
 
     @abstractmethod
-    def find(self, ndi_query):
+    def find(self, query):
         """It should be able to utilize a :term:`NDI query` to retrive data from the :term:`collection` of the given :term:`NDI class`.
 
-        :param ndi_query: A set of conditions to find on. Not passing a query will return all :term:`document`\ s from the :term:`collection.
-        :type ndi_query: :term:`NDI query`, optional
+        :param query: A set of conditions to find on. Not passing a query will return all :term:`document`\ s from the :term:`collection.
+        :type query: :term:`NDI query`, optional
         """
         pass
 
     @abstractmethod
-    def update_many(self, ndi_query, payload, force=False):
+    def update_many(self, query, payload, force=False):
         """It should be able to update all :term:`document`\ s matching the given :term:`NDI query` in the :term:`collection` of the given :term:`NDI class`.
 
         :param ndi_class:
         :type ndi_class: :class:`NDI_Object`
-        :param ndi_query: A set of conditions to update on. Not passing a query will update all :term:`document`\ s from the :term:`collection`.
-        :type ndi_query: :term:`NDI query`, optional
+        :param query: A set of conditions to update on. Not passing a query will update all :term:`document`\ s from the :term:`collection`.
+        :type query: :term:`NDI query`, optional
         :param payload: Containing fields with values to update to in the specified ndi_class. Not passing a dict will result in no updates.
         :type payload: :term:`payload`, optional
         """
@@ -116,7 +116,7 @@ class NDI_Database(ABC):
 
         :param ndi_class:
         :type ndi_class: :class:`NDI_Object`
-        :param ndi_query: A set of conditions to delete on. Not passing a query will delete all :term:`document`\ s from the :term:`collection`.
-        :type ndi_query: :term:`NDI query`, optional
+        :param query: A set of conditions to delete on. Not passing a query will delete all :term:`document`\ s from the :term:`collection`.
+        :type query: :term:`NDI query`, optional
         """
         pass
