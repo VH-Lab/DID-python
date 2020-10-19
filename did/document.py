6fdd1d02532160aa4a49291706ac9095f0180ae8
David Nishikawa <david@squishymedia.com>
2020-10-18 17:20:59 -0700

    extract did_document id more safely

--- did/document.py
+++ did/document.py
@@ -1,7 +1,10 @@
 class DIDDocument:
     def __init__(self, data):
-        self.id = data.base['id']
-        self.data = data
+        try:
+            self.id = data.get('base').get('id') 
+            self.data = data
+        except AttributeError:
+            raise Exception('DIDDocuments must be instantiated with a did_document in dict format.')
 
     @property
     def binary_files(self):
