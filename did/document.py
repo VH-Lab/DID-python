c59fb46b7227e3a1cca6de002d71ab9933f4472e
Peter Zhao <peterzhao204@gmail.com>
2021-01-26 12:59:50 -0500

    new methods added

--- did/document.py
+++ did/document.py
@@ -2,12 +2,14 @@ import json
 import os
 import struct
 import random
-from .globals import did_documentpath, did_schemapath
+import copy
+from .globals import did_documentpath, did_schemapath, parse_didpath
 from datetime import datetime as dt
 
 
 class DIDDocument:
 	def __init__(self, data=None):
+		self.superclasses_docs = {}
 		try:
 			self.id = data.get('base').get('id')
 			self.data = data
@@ -24,36 +26,52 @@ class DIDDocument:
 		:param document_type:
 		:return:
 		"""
+
 		def __make_id__():
 			date = dt.now()
 			integral_part = date.toordinal()
-			fractional_part = (date - date.fromordinal(integral_part)).total_seconds()/(24*60*60)
-			serial_date_number = hex(struct.unpack('!q', struct.pack('!d', integral_part + fractional_part))[0])
-			random_number = random.random() + random.randint(-32727, 32727)
-			hex_random_number = hex(struct.unpack('!q', struct.pack('!d', random_number))[0])
-			return "{}_{}".format(serial_date_number, hex_random_number)
+			fractional_part = (date - date.fromordinal(integral_part)).total_seconds() / (24 * 60 * 60)
+			serial_date_number = struct.unpack('!q', struct.pack('!d', integral_part + fractional_part))[0]
+			hex_serial_date_number = format(serial_date_number, 'x')
+			random_number = abs(random.random() + random.randint(-32727, 32727))
+			hex_random_number = format(struct.unpack('!q', struct.pack('!d', random_number))[0], 'x')
+			return "{}_{}".format(hex_serial_date_number, hex_random_number)
 
-		data = {'base':
-			{
-				'id': '',
-				'session_id': __make_id__(),
-				'name': '',
-				'datestamp': '',
-				'database_version': 1
-			},
+		base = DIDDocument(data={
+			'base':
+				{
+					'id': __make_id__(),
+					'session_id': '',
+					'name': '',
+					'datestamp': dt.now().isoformat(),
+					'database_version': 1
+				},
 			'class':
 				{
-					'definition': "$DIDDOCUMENTPATH/{}.json".format(document_type),
-					'validation': "$DIDSCHEMAPATH/{}.json".format(document_type),
-					'class_name': document_type,
-					'property_list_name': document_type,
+					'definition': "$DIDDOCUMENTPATH/base.json",
+					'validation': "$DIDSCHEMAPATH/base.json",
+					'class_name': 'did_base',
+					'property_list_name': 'base',
 					'class_version': 1,
-					'superclasses': [{'definition': "$DIDDOCUMENTPATH/base.json",
-									  'version': 1}]
+					'superclasses': []
 				},
-			document_type: {}
-		}
-		return cls(data=data)
+		})
+		data = {'base': base.data,
+				'class':
+					{
+						'definition': "$DIDDOCUMENTPATH/{}.json".format(document_type),
+						'validation': "$DIDSCHEMAPATH/{}.json".format(document_type),
+						'class_name': document_type,
+						'property_list_name': document_type,
+						'class_version': 1,
+						'superclasses': [{'definition': "$DIDDOCUMENTPATH/base.json",
+										  'version': 1}]
+					},
+				document_type: {}
+				}
+		doc = cls(data=data)
+		doc.superclasses_docs['base'] = base
+		return doc
 
 	@classmethod
 	def from_json(cls, document_type, starting_path=did_documentpath()):
@@ -67,35 +85,36 @@ class DIDDocument:
 								the script is run
 		:return: an instance of DIDDocument
 		"""
+
 		def __search__(path, fname):
 			for _dirpath, _dirname, _filename in os.walk(path):
 				if os.path.isfile(os.path.join(_dirpath, fname)):
 					return _dirpath, fname
 				else:
 					for directory in _dirname:
-						return __search__(os.path.join(_dirpath, directory))
+						try:
+							return __search__(os.path.join(_dirpath, directory))
+						except FileNotFoundError:
+							continue
 					raise FileNotFoundError('{} cannot found from {}'.format(fname, path))
 
 		def __readjsonfromblankfile__(jsonstr):
 			properties = json.loads(jsonstr)
+			properties[properties.get('document_class').get('property_list_name')] = properties
 			for superclass in properties.get('document_class', default={'superclass': []}).get('superclasses',
 																							   default=[]):
-				doc = cls.from_json(document_type, starting_path)
-				properties[superclass] = doc.serialize()[superclass]
+				with open(parse_didpath(superclass['definition'])) as f:
+					doc = json.load(f)
+				doc = DIDDocument(data=doc)
+				properties[superclass] = doc.data[doc.property_list_name]
 			return properties
 
 		if not document_type.endswith('.json'):
 			document_type += '.json'
-		try:
-			f = open(os.path.join(starting_path, document_type))
-			try:
-				properties = __readjsonfromblankfile__(f.read())
-				return cls(data={'document_properties': properties})
-			finally:
-				f.close()
-		except FileNotFoundError:
-			dirpath, fname = __search__(starting_path, document_type)
-			return cls.from_json(document_type=fname, starting_path=dirpath)
+		dirpath, fname = __search__(starting_path, document_type)
+		with open(os.path.join(dirpath, fname)) as f:
+			properties = __readjsonfromblankfile__(f.read())
+			return cls(data=properties)
 
 	@property
 	def binary_files(self):
@@ -207,9 +226,73 @@ class DIDDocument:
 		return self.data.get('class').get('property_list_name')
 
 	@property
+	def property_list(self, class_name=None):
+		if class_name is None:
+			properties = self.data[self.property_list_name]
+			for superclass in self.superclasses_docs:
+				superclass_property = self.superclasses_docs[superclass].data[self.superclasses_docs[superclass].property_list_name]
+				for key in superclass_property:
+					properties[key] = superclass_property[key]
+			return copy.deepcopy(properties)
+		else:
+			if class_name not in self.superclasses_docs or class_name != self.property_list_name:
+				raise ValueError('class_name is not the class nor the superclasses of the document')
+			else:
+				if class_name in self.superclasses_docs:
+					return copy.deepcopy(self.superclasses_docs[class_name][self.superclasses_docs[class_name].property_list_name])
+				else:
+					return copy.deepcopy(self.data[self.property_list_name])
+
+	@property
 	def class_version(self):
 		return self.data.get('class').get('class_version')
 
 	@property
 	def superclasses(self):
-		return self.data.get('class').get('super_classes')
+		return self.data.get('class').get('superclasses')
+
+	def add_property(self, field, value=''):
+		self.data[self.property_list_name][field] = value
+
+	def add_new_superclass(self, datatype):
+		self.superclasses[datatype] = self.make_blankdocument(datatype)
+		self.data = self.superclasses[datatype].data[self.superclasses[datatype].property_list_name]
+
+	def save(self):
+		"""
+		Generate a json file in the directory as specified in the NDIDOCUMENTPATH, which
+		serves as a template for instantiate a document of such data type
+		"""
+		def __maketemplate__(data):
+			if isinstance(data, type('')):
+				return ''
+			if isinstance(data, type([])):
+				return []
+			if isinstance(data, type(0)):
+				return -1
+			if isinstance(data, type({})):
+				replaced = {}
+				for key in data:
+					replaced[key] = __maketemplate__(data[key])
+				return replaced
+
+		for superclass in self.superclasses_docs:
+			if not os.path.isfile(parse_didpath(self.superclasses_docs[superclass].class_definition)):
+				self.superclasses_docs[superclass].save(self.superclasses_docs[superclass].class_definition)
+			else:
+				print("{} already exists".format(self.superclasses_docs[superclass].class_definition))
+				print()
+		full_directory = parse_didpath(self.class_definition)
+		directory = os.path.join(*(os.path.split(full_directory)[:-1]))
+		if not os.path.isdir(directory):
+			os.mkdir(directory)
+		f = open(full_directory, "w")
+		data = {'class': self.data['class'],
+				self.property_list_name: __maketemplate__(self.data[self.property_list_name])}
+		try:
+			print("Generating json file for the class at {}".format(self.class_definition))
+			print(data)
+			print()
+			json.dump(data, f)
+		finally:
+			f.close()
