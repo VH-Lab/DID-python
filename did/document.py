4b7b187e00afae9f76ea07170966bdc02790d0f8
Peter Zhao <peterzhao204@gmail.com>
2021-01-25 19:37:16 -0500

    alternative constructors added

--- did/document.py
+++ did/document.py
@@ -1,17 +1,215 @@
 import json
+import os
+import struct
+import random
+from .globals import did_documentpath, did_schemapath
+from datetime import datetime as dt
+
 
 class DIDDocument:
-    def __init__(self, data):
-        try:
-            self.id = data.get('base').get('id') 
-            self.data = data
-        except AttributeError:
-            raise Exception('DIDDocuments must be instantiated with a did_document in dict format.')
+	def __init__(self, data=None):
+		try:
+			self.id = data.get('base').get('id')
+			self.data = data
+		except AttributeError:
+			raise Exception('DIDDocuments must be instantiated with a did_document in dict format.')
 
-    @property
-    def binary_files(self):
-        """ Get binary filenames """
-        return self.data.get('binary_files')
+	def serialize(self):
+		return json.dumps(self.data)
 
-    def serialize(self):
-        return json.dumps(self.data)
+	@classmethod
+	def make_blankdocument(cls, document_type):
+		"""
+
+		:param document_type:
+		:return:
+		"""
+		def __make_id__():
+			date = dt.now()
+			integral_part = date.toordinal()
+			fractional_part = (date - date.fromordinal(integral_part)).total_seconds()/(24*60*60)
+			serial_date_number = hex(struct.unpack('!q', struct.pack('!d', integral_part + fractional_part))[0])
+			random_number = random.random() + random.randint(-32727, 32727)
+			hex_random_number = hex(struct.unpack('!q', struct.pack('!d', random_number))[0])
+			return "{}_{}".format(serial_date_number, hex_random_number)
+
+		data = {'base':
+			{
+				'id': '',
+				'session_id': __make_id__(),
+				'name': '',
+				'datestamp': '',
+				'database_version': 1
+			},
+			'class':
+				{
+					'definition': "$DIDDOCUMENTPATH/{}.json".format(document_type),
+					'validation': "$DIDSCHEMAPATH/{}.json".format(document_type),
+					'class_name': document_type,
+					'property_list_name': document_type,
+					'class_version': 1,
+					'superclasses': [{'definition': "$DIDDOCUMENTPATH/base.json",
+									  'version': 1}]
+				},
+			document_type: {}
+		}
+		return cls(data=data)
+
+	@classmethod
+	def from_json(cls, document_type, starting_path=did_documentpath()):
+		"""
+		return the text from a json file location string in NDI. It looks for the corresponding
+		JSON file in the starting_path as well as those in all subdirectory of the starting_path
+
+		:param document_type: 	the name of the json file
+		:param starting_path: 	the path from where the json file will be looked for; by
+								default it is set to DIDDOCUMENTPATH in the .env file of where
+								the script is run
+		:return: an instance of DIDDocument
+		"""
+		def __search__(path, fname):
+			for _dirpath, _dirname, _filename in os.walk(path):
+				if os.path.isfile(os.path.join(_dirpath, fname)):
+					return _dirpath, fname
+				else:
+					for directory in _dirname:
+						return __search__(os.path.join(_dirpath, directory))
+					raise FileNotFoundError('{} cannot found from {}'.format(fname, path))
+
+		def __readjsonfromblankfile__(jsonstr):
+			properties = json.loads(jsonstr)
+			for superclass in properties.get('document_class', default={'superclass': []}).get('superclasses',
+																							   default=[]):
+				doc = cls.from_json(document_type, starting_path)
+				properties[superclass] = doc.serialize()[superclass]
+			return properties
+
+		if not document_type.endswith('.json'):
+			document_type += '.json'
+		try:
+			f = open(os.path.join(starting_path, document_type))
+			try:
+				properties = __readjsonfromblankfile__(f.read())
+				return cls(data={'document_properties': properties})
+			finally:
+				f.close()
+		except FileNotFoundError:
+			dirpath, fname = __search__(starting_path, document_type)
+			return cls.from_json(document_type=fname, starting_path=dirpath)
+
+	@property
+	def binary_files(self):
+		""" Get binary filenames """
+		return self.data.get('binary_files')
+
+	@property
+	def session_id(self):
+		"""
+		A globally unique identifier for the experimental session. Once made, this never changes; even if
+		version is updated.
+
+		:return: did_id string
+		"""
+		return self.data.get('base').get('session_id')
+
+	@session_id.setter
+	def session_id(self, id):
+		"""
+		setter for session_id
+
+		:param id:
+		"""
+		self.data['base']['session_id'] = str(id)
+
+	@property
+	def name(self):
+		"""
+		A string name for the user. Does not need to be unique. The id, session_id, and version confer uniqueness.
+		(Some subtypes may have conditions for name uniqueness; for example, daq_systems must have a unique name.
+		But this is not a database-level requirement.)
+
+		:return: ASCII string
+		"""
+		return self.data.get('base').get('name')
+
+	@name.setter
+	def name(self, name):
+		"""
+		setter for session_id
+
+		:param name:
+		"""
+		self.data['base']['name'] = str(name)
+
+	@property
+	def datestamp(self):
+		"""
+		Time of document creation or modification (that is, it is updated when version is updated)
+
+		:return: ISO-8601 date string, time zone must be UTC leap seconds
+		"""
+		return self.data.get('base').get('datestamp')
+
+	@property
+	def database_version(self):
+		"""
+		This probably needs to be an did_id string to help with merging branches where 2 users have modified the same
+		database entry (otherwise, might have two copies of "n+1" that need to be dealt with); did_id strings sort by
+		time alphabetically, so the time would be a means of differentiating them
+
+		:return: did_id string
+		"""
+		return self.data.get('base').get('database_version')
+
+	@property
+	def class_definition(self):
+		"""
+		JSON_definition_location of the definition for this document
+
+		:return: path like string
+		"""
+		return self.data.get('class').get('definition')
+
+	@property
+	def class_validation(self):
+		"""
+		JSON_schema_location of the schema validation for this document
+
+		:return: path like string
+		"""
+		return self.data.get('class').get('validation')
+
+	@property
+	def class_name(self):
+		"""
+		Name of this document class
+
+		:return: string
+		"""
+		return self.data.get('class').get('class_name')
+
+	@class_name.setter
+	def class_name(self, name):
+		"""
+		setter for class_name
+
+		:param name:
+		"""
+		self.class_name['class']['class_name'] = str(name)
+
+	@property
+	def property_list_name(self):
+		"""
+		String that describes the Property list that is provided by this class
+
+		:return: string
+		"""
+		return self.data.get('class').get('property_list_name')
+
+	@property
+	def class_version(self):
+		return self.data.get('class').get('class_version')
+
+	@property
+	def superclasses(self):
+		return self.data.get('class').get('super_classes')
