9edb848c7f4d9d99b32ba6cfe33d315ba172dd89
Peter Zhao <peterzhao204@gmail.com>
2021-02-22 23:58:38 -0500

    added integration tests

--- tests/database/mongo/test_mongo_database.py
+++ tests/database/mongo/test_mongo_database.py
@@ -57,3 +57,46 @@ class TestMongoDB:
         did.save("Update app # 7 to new app name")
         assert(len(did.get_history())) == 3
         assert(did.get_history()[0]['message'] == "Update app # 7 to new app name")
+    
+    def test_search_heavy_operations(self, did, mock_documents):
+        windows = [doc for doc in mock_documents if 'app' in doc.data and doc.data['app']['os'] == 'Windows']
+        mac_os = [doc for doc in mock_documents if 'app' in doc.data and doc.data['app']['os'] == 'Mac OS']
+        linux = [doc for doc in mock_documents if 'app' in doc.data and doc.data['app']['os'] == 'Linux']
+        for app in windows:
+            did.add(app)
+        snapshot1_id = did.db.working_snapshot_id
+        docs = did.find(Query("app.os") == 'Windows')
+        assert len(docs) == len(windows)
+        did.save()
+        assert len(did.find(Query("app.os") == 'Windows', snapshot=snapshot1_id)) == len(windows)
+        assert len(did.find(Query("app.os") == 'Windows', snapshot=snapshot1_id, commit="1234563")) == len(windows)
+        for app in mac_os:
+            did.add(app)
+        docs = did.find(Query("app.os") == 'Linux')
+        assert len(docs) == 0
+        docs = did.find(Query("app.os") == 'Windows')
+        assert len(docs) == len(windows)
+        for app in windows:
+            did.delete(app)
+        did.save()
+        docs = did.find(Query("app.os") == 'Windows')
+        assert len(docs) == 0
+        docs = did.find(Query("app.os") == 'Windows', in_all_history=True)
+        assert len(docs) == len(windows)
+        history = did.get_history()
+        commit_hash = history[1]['commit_hash']
+        docs = did.find(Query("app.os") == 'Windows', commit=commit_hash)
+        assert len(docs) == len(windows)
+        for app in windows:
+            did.upsert(app)
+        for app in mac_os:
+            did.upsert(app)
+        for app in linux:
+            did.upsert(app)
+        assert len(did.find(Query("app.os") == 'Windows')) == len(windows)
+        assert len(did.find(Query("app.os") == 'Mac OS')) == len(mac_os)
+        assert len(did.find(Query("app.os") == 'Linux')) == len(linux)
+        did.revert()
+        assert len(did.find(Query("app.os") == 'Windows')) == 0
+        assert len(did.find(Query("app.os") == 'Mac OS')) == len(mac_os)
+        assert len(did.find(Query("app.os") == 'Linux')) == 0
