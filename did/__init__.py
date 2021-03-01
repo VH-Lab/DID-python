991a47fac74d999d5945517594bffef698750bea
Peter Zhao <peterzhao204@gmail.com>
2021-03-01 12:04:46 -0500

    Update __init__.py

--- did/__init__.py
+++ did/__init__.py
@@ -4,5 +4,5 @@ from .core import DID
 from .settings import (
     get_db_connection_string, get_documentpath,
     get_schemapath, set_database_connection_string, 
-    set_documentpath, set_schemapath)
+    set_documentpath, set_schemapath, set_variable, list_variables)
 from .id import DIDId
