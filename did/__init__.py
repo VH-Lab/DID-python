9f038ffc3c1118ee89762626d52eb7ddd1e1a168
Peter Zhao <peterzhao204@gmail.com>
2021-03-01 01:26:49 -0500

    updated import

--- did/__init__.py
+++ did/__init__.py
@@ -1,6 +1,8 @@
 from .document import DIDDocument
 from .query import Query
 from .core import DID
-from .tools import settings
-from .tools.settings import get_db_connection_string, get_documentpath, \
-get_schemapath, set_database_connection_string, set_documentpath, set_schemapath
+from .settings import (
+    get_db_connection_string, get_documentpath,
+    get_schemapath, set_database_connection_string, 
+    set_documentpath, set_schemapath)
+from .id import DIDId
